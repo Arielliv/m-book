bc0e5ed4ae8f1fb1a74a4f44cae90d84fb2a3a17
ariel7342 <ariel7342@gmail.com>
2015-11-13 20:29:57 +0200

    6 - playing witj some photos

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,33 +11,20 @@ var app = angular.module('app', [ 'ui.bootstrap' ,'restangular','ngSanitize']);
             };
         }]);
         $scope.Cards = [];
-        $scope.counterAddCards = 0;
         $scope.search = false;
-       /* $scope.addToViewCard= function(){
-            $scope.htmlTest = '<div class="panel col-lg-1">' +
-                '<lable class="card col-lg-12 col-lg-offset-1">{{selectedCard.monitorName}}</lable>' +
-                '<img src="images/oracleDB.jpg">'+
-                '</div>';
-
-            for(var i = 1;i < $scope.Cards.length; i++){
-                $scope.htmlTest = $scope.htmlTest + '<div class="panel col-lg-1">' +
-                    '<lable class="card col-lg-12 col-lg-offset-1">'+'{{' + selectedCard.monitorName +'}}'+'</lable>' +
-                    '<img src="images/oracleDB.jpg">'+
-                    '</div>';
-            }
-        };*/
         $scope.addCard = function(){
             var modalInstance = $uibModal.open({
                 templateUrl: 'addCardView.html',
                 controller: 'addCardController'
             });
             modalInstance.result.then(function(newCard) {
-                $scope.counterAddCards ++;
-                newCard.$index = $scope.counterAddCards;
-
-                        newCard.img = "images/oracleDB.jpg";
+                if(newCard.monitorProdact == "not exist here"){
+                    newCard.monitorProdact = "default";
+                    newCard.img = "images/background4.jpg"
+                } else {
+                    newCard.img ="images/" + newCard.monitorProdact + ".jpg";
+                }
                 $scope.Cards.push(newCard);
-                console.log($scope.counterAddCards);
             });
         };
         $scope.openCard = function(selectedCard){
@@ -53,35 +40,9 @@ var app = angular.module('app', [ 'ui.bootstrap' ,'restangular','ngSanitize']);
             });
         };
     });
-/*app.directive('ngCreateMonitorCard',function(){
-    return{
-        restrict: 'E',
-        template:function($scope, elemnt, attr) {
-            var img = "images/oracleDB.jpg";
-            var htmlTest = '';
-            $scope.counterAddCards = 0;
-            var y = $scope.counterAddCards;
-            console.log($scope.counterAddCards);
-            for(var i = 0;i < y; i++){
-                console.log($scope.counterAddCards);
-                htmlTest += '<ng-monitor-card class="panel col-lg-1">' +
-                    '<lable class="card col-lg-12 col-lg-offset-1">{{newCard.monitorName}}</lable>' +
-                    '<img  src=' + img + '>' + '</ng-monitor-card>';
-            }
-            return htmlTest;
-        }
 
-    };
-});
-app.directive('ngMonitorCard',['$uibModal',function($uibModal) {
-    return {
-        link: function($scope, element, attr) {
-            element.bind('click',function(){
 
-            });
-        }
-    };
-}]);*/
+
 
 
 
